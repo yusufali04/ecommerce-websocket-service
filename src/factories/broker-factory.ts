ffbf9492b8042fb61fd72dfeb844dabf5b72a280
Yusuf <yusufali.5094@gmail.com>
2025-09-18 23:01:33 +0530

    initial commit

---
+++ src/factories/broker-factory.ts
@@ -0,0 +1,14 @@
+import config from "config";
+import { KafkaBroker } from "../config/kafka";
+import { MessageBroker } from "../types/broker";
+
+let broker: MessageBroker | null = null;
+
+export const createMessageBroker = (): MessageBroker => {
+  console.log("connecting to kafka broker...");
+  // singleton
+  if (!broker) {
+    broker = new KafkaBroker("websocket-service", [config.get("kafka.broker")]);
+  }
+  return broker;
+};
